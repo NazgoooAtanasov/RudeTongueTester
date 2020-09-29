94a19c64331f7a27f4b723ef9b2d958a0a123cdd
nazgo <naskodragomirov@gmail.com>
2020-09-28 18:19:58 -0700

    Implemented user friendly result messages.

--- src/RudeTongueTester/RudeTongueTester.IO/Writer/Writer.cs
+++ src/RudeTongueTester/RudeTongueTester.IO/Writer/Writer.cs
@@ -5,6 +5,16 @@ namespace RudeTongueTester.IO.Writer
     public class Writer : IWriter
     {
         public void Write(string args)
-            => Console.WriteLine(args);
+            => Console.WriteLine(this.FormatOutput(args));
+
+        private string FormatOutput(string output)
+        {
+            return output switch
+            {
+                "1" => "This text is \"rude\".",
+                "0" => "This text is not \"rude\".",
+                _ => "Error"
+            };
+        }
     }
 }
